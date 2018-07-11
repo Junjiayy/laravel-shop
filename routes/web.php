56a75580e625fe5c73b69b1d043e12c00732fe54
张俊加 <1020446694@qq.com>
2018-07-11 14:15:31 +0800

    用户界面-商品详情

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 /* 用户登录后可执行操作路由 */
